60afc07c26bb155a92be3f885f2d8ab9b8897bc6
vsepulve <vsepulve@dcc.uchile.cl>
2018-04-25 15:47:21 -0300

    Basecode for method StartSimulation added (it now receives the json and sends it to the server, and then receives its answer before returning).

--- models/crud.go
+++ models/crud.go
@@ -9,6 +9,7 @@ import (
 	"io"
 	"bytes"
 	"net/http"
+	"encoding/json"
 )
 
 func Setup(app *gin.Engine) {
@@ -100,6 +101,61 @@ func TestSimulation(c *gin.Context) {
 func StartSimulation(c *gin.Context) {
 	
 	fmt.Printf("StartSimulation - Inicio\n")
+	
+	var data interface{}
+	e := c.BindJSON(&data)
+	if e != nil {
+		panic(e)
+	}
+	
+	json_text, e := json.MarshalIndent(data, "", "\t")
+//	json_text, e := json.Marshal(data)
+	
+	fmt.Printf("StartSimulation - Data: %s\n", json_text)
+	
+	connection, error := net.Dial("tcp", "localhost:12345")
+	if error != nil {
+		fmt.Println(error)
+	}
+	defer connection.Close()
 
+	// Envio el tipo de request (un byte con un valor arbitrario, 7 en este caso)
+	fmt.Printf("\nStartSimulation - Enviando request_type (7)\n")
+	request_type := []byte{7}
+	connection.Write(request_type)
+	
+	// Envio el mensaje de prueba (string en el formato length + chars)
+	// Notar que esto, si o si, implica convertir un numero en binario
+//	message := "Prueba"
+	message := string(json_text)
+	length := len(message)
+	
+	fmt.Printf("StartSimulation - Enviando length (%d)\n", length)
+	bytes_int := make([]byte, 4)
+	binary.LittleEndian.PutUint32(bytes_int, uint32(length))
+	connection.Write(bytes_int)
+	
+	fmt.Printf("StartSimulation - Enviando mensaje\n")
+	connection.Write([]byte(strings.TrimRight(message, "\n")))
+	
+	// Recibo la respuesta de prueba (un entero en 4 bytes)
+	fmt.Printf("StartSimulation - Leyendo codigo de respuesta\n")
+	
+	var buf bytes.Buffer
+	io.Copy(&buf, connection)
+	resp_code := binary.LittleEndian.Uint32(buf.Bytes())
+	fmt.Printf("TestSimulation - Respuesta: %d (%d bytes)\n", resp_code, buf.Len())
+	
+	fmt.Printf("\n TestSimulation - Fin\n")
+	
+	resp := make(map[string]string)
+	
+	resp["Campo Prueba"] = "Valor Prueba"
+	resp["Estado"] = "Ok"
+	c.JSON(http.StatusOK, resp)
 
 }
+
+
+
+
